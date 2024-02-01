f76de314cae810197ca76ad5b9000a59101b5d94
Lennart Ploom <lennart.ploom@guardtime.com>
2024-02-01 11:45:42 +0200

    implement jsonrpc client functions

--- client/rpc/client.go
+++ client/rpc/client.go
@@ -2,18 +2,22 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/alphabill-org/alphabill/rpc"
+	"github.com/alphabill-org/alphabill/types"
+	ethrpc "github.com/ethereum/go-ethereum/rpc"
+	"github.com/fxamacker/cbor/v2"
 )
 
 // Client defines typed wrappers for the Alphabill RPC API.
 type Client struct {
-	c *rpc.Client
+	c *ethrpc.Client
 }
 
 // DialContext connects a client to the given URL with context.
 func DialContext(ctx context.Context, url string) (*Client, error) {
-	c, err := rpc.DialContext(ctx, url)
+	c, err := ethrpc.DialContext(ctx, url)
 	if err != nil {
 		return nil, err
 	}
@@ -21,7 +25,7 @@ func DialContext(ctx context.Context, url string) (*Client, error) {
 }
 
 // NewClient creates a client that uses the given RPC client.
-func NewClient(c *rpc.Client) *Client {
+func NewClient(c *ethrpc.Client) *Client {
 	return &Client{c}
 }
 
@@ -31,7 +35,7 @@ func (c *Client) Close() {
 }
 
 // Client gets the underlying RPC client.
-func (c *Client) Client() *rpc.Client {
+func (c *Client) Client() *ethrpc.Client {
 	return c.c
 }
 
@@ -40,4 +44,78 @@ func (c *Client) GetRoundNumber(ctx context.Context) (uint64, error) {
 	var num uint64
 	err := c.c.CallContext(ctx, &num, "state_getRoundNumber")
 	return num, err
+}
+
+// GetUnit returns the unit data for given unitID.
+func (c *Client) GetUnit(ctx context.Context, unitID []byte, returnProof, returnData bool) (*types.UnitDataAndProof, error) {
+	var res *rpc.UnitData
+	if err := c.c.CallContext(ctx, &res, "state_getUnit", unitID, returnProof, returnData); err != nil {
+		return nil, err
+	}
+	var unitData *types.UnitDataAndProof
+	if err := cbor.Unmarshal(res.DataAndProofCBOR, &unitData); err != nil {
+		return nil, fmt.Errorf("failed to decode unit data cbor: %w", err)
+	}
+	return unitData, nil
+}
+
+// GetUnitsByOwnerID returns list of unit identifiers that belong to the given owner.
+func (c *Client) GetUnitsByOwnerID(ctx context.Context, ownerID []byte) ([]types.UnitID, error) {
+	var res []types.UnitID
+	err := c.c.CallContext(ctx, &res, "state_getUnitsByOwnerID", ownerID)
+	return res, err
+}
+
+// SendTransaction broadcasts the given transaction to the network, returns the submitted transaction hash.
+func (c *Client) SendTransaction(ctx context.Context, tx *types.TransactionOrder) ([]byte, error) {
+	txCbor, err := encodeCbor(tx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode transaction to cbor: %w", err)
+	}
+	var res []byte
+	err = c.c.CallContext(ctx, &res, "state_sendTransaction", &rpc.Transaction{TxOrderCbor: txCbor})
+	return res, err
+}
+
+// GetTransactionProof returns transaction record and proof for the given transaction hash.
+func (c *Client) GetTransactionProof(ctx context.Context, txHash []byte) (*types.TransactionRecord, *types.TxProof, error) {
+	var res *rpc.TransactionRecordAndProof
+	err := c.c.CallContext(ctx, &res, "state_getTransactionProof", txHash)
+	if err != nil {
+		return nil, nil, err
+	}
+	var txRecord *types.TransactionRecord
+	if err = cbor.Unmarshal(res.TxRecordCbor, &txRecord); err != nil {
+		return nil, nil, fmt.Errorf("failed to decode tx record: %w", err)
+	}
+	var txProof *types.TxProof
+	if err = cbor.Unmarshal(res.TxProofCbor, &txProof); err != nil {
+		return nil, nil, fmt.Errorf("failed to decode tx proof: %w", err)
+	}
+	return txRecord, txProof, nil
+}
+
+// GetBlock returns block for given round number.
+func (c *Client) GetBlock(ctx context.Context, roundNumber uint64) (*types.Block, error) {
+	var res *rpc.Block
+	if err := c.c.CallContext(ctx, &res, "state_getBlock", roundNumber); err != nil {
+		return nil, err
+	}
+	var block *types.Block
+	if err := cbor.Unmarshal(res.BlockCbor, &block); err != nil {
+		return nil, fmt.Errorf("failed to decode block: %w", err)
+	}
+	return block, nil
+}
+
+func encodeCbor(v interface{}) ([]byte, error) {
+	enc, err := cbor.CanonicalEncOptions().EncMode()
+	if err != nil {
+		return nil, err
+	}
+	data, err := enc.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
--- client/rpc/client_test.go
+++ client/rpc/client_test.go
@@ -2,32 +2,164 @@ package rpc
 
 import (
 	"context"
+	"crypto"
 	"errors"
 	"net"
 	"net/http"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/alphabill-org/alphabill/predicates"
+	"github.com/alphabill-org/alphabill/rpc"
+	"github.com/alphabill-org/alphabill/types"
+	ethrpc "github.com/ethereum/go-ethereum/rpc"
+	"github.com/fxamacker/cbor/v2"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRpcClient(t *testing.T) {
-	service := &mockService{roundNumber: 1337}
+	service := &mockService{}
 	client := startServerAndClient(t, service)
+
 	t.Run("GetRoundNumber_OK", func(t *testing.T) {
+		service.reset()
+		service.roundNumber = 1337
+
 		roundNumber, err := client.GetRoundNumber(context.Background())
 		require.NoError(t, err)
 		require.EqualValues(t, 1337, roundNumber)
 	})
 	t.Run("GetRoundNumber_NOK", func(t *testing.T) {
-		service.roundNumberErr = errors.New("some error")
+		service.reset()
+		service.err = errors.New("some error")
+
 		_, err := client.GetRoundNumber(context.Background())
 		require.ErrorContains(t, err, "some error")
+	})
+
+	t.Run("GetUnit_OK", func(t *testing.T) {
+		service.reset()
+		unitID := []byte{1}
+		unitAndProof := &types.UnitDataAndProof{
+			UnitData: &types.StateUnitData{
+				Data:   cbor.RawMessage{0x81, 0x00},
+				Bearer: predicates.PredicateBytes{0x83, 0x00, 0x01, 0xF6},
+			},
+			Proof: &types.UnitStateProof{
+				UnitID: unitID,
+			},
+		}
+		unitAndProofCbor, err := encodeCbor(unitAndProof)
+		require.NoError(t, err)
+		service.unitData = &rpc.UnitData{DataAndProofCBOR: unitAndProofCbor}
+
+		unitData, err := client.GetUnit(context.Background(), unitID, true, true)
+		require.NoError(t, err)
+		require.Equal(t, unitAndProof, unitData)
+	})
+	t.Run("GetUnit_NOK", func(t *testing.T) {
+		service.reset()
+		service.err = errors.New("some error")
+		unitID := []byte{1}
+
+		unitData, err := client.GetUnit(context.Background(), unitID, true, true)
+		require.ErrorContains(t, err, "some error")
+		require.Nil(t, unitData)
+	})
+
+	t.Run("GetUnitsByOwnerID_OK", func(t *testing.T) {
+		service.reset()
+		ownerID := []byte{1}
+		unitID1 := []byte{2}
+		unitID2 := []byte{3}
+		service.ownerUnitIds = []types.UnitID{unitID1, unitID2}
+
+		unitIDs, err := client.GetUnitsByOwnerID(context.Background(), ownerID)
+		require.NoError(t, err)
+		require.Equal(t, service.ownerUnitIds, unitIDs)
+	})
+	t.Run("GetUnitsByOwnerID_NOK", func(t *testing.T) {
+		service.reset()
+		service.err = errors.New("some error")
+		ownerID := []byte{1}
+
+		unitData, err := client.GetUnitsByOwnerID(context.Background(), ownerID)
+		require.ErrorContains(t, err, "some error")
+		require.Nil(t, unitData)
+	})
+
+	t.Run("SendTransaction_OK", func(t *testing.T) {
+		service.reset()
+		unitID := []byte{1}
+		tx := &types.TransactionOrder{Payload: &types.Payload{UnitID: unitID}}
+
+		txHash, err := client.SendTransaction(context.Background(), tx)
+		require.NoError(t, err)
+		require.Equal(t, tx.Hash(crypto.SHA256), txHash)
+	})
+	t.Run("SendTransaction_NOK", func(t *testing.T) {
+		service.reset()
+		service.err = errors.New("some error")
+		unitID := []byte{1}
+		tx := &types.TransactionOrder{Payload: &types.Payload{UnitID: unitID}}
+
+		txHash, err := client.SendTransaction(context.Background(), tx)
+		require.ErrorContains(t, err, "some error")
+		require.Nil(t, txHash)
+	})
+
+	t.Run("GetTransactionProof_OK", func(t *testing.T) {
+		service.reset()
+		txHash := []byte{1}
+		unitID := []byte{1}
+		txRecord := &types.TransactionRecord{TransactionOrder: &types.TransactionOrder{Payload: &types.Payload{UnitID: unitID}}}
+		txProof := &types.TxProof{}
+		txRecordCbor, err := encodeCbor(txRecord)
+		require.NoError(t, err)
+		txProofCbor, err := encodeCbor(txProof)
+		require.NoError(t, err)
+		service.txProof = &rpc.TransactionRecordAndProof{TxRecordCbor: txRecordCbor, TxProofCbor: txProofCbor}
+
+		txRecordRes, txProofRes, err := client.GetTransactionProof(context.Background(), txHash)
+		require.NoError(t, err)
+		require.Equal(t, txRecord, txRecordRes)
+		require.Equal(t, txProof, txProofRes)
+	})
+	t.Run("GetTransactionProof_NOK", func(t *testing.T) {
+		service.reset()
+		service.err = errors.New("some error")
+		txHash := []byte{1}
+
+		txr, txp, err := client.GetTransactionProof(context.Background(), txHash)
+		require.ErrorContains(t, err, "some error")
+		require.Nil(t, txr)
+		require.Nil(t, txp)
+	})
+
+	t.Run("GetBlock_OK", func(t *testing.T) {
+		service.reset()
+		unitID := []byte{1}
+		txRecord := &types.TransactionRecord{TransactionOrder: &types.TransactionOrder{Payload: &types.Payload{UnitID: unitID}}}
+		block := &types.Block{Transactions: []*types.TransactionRecord{txRecord}}
+		blockCbor, err := encodeCbor(block)
+		require.NoError(t, err)
+		service.block = &rpc.Block{BlockCbor: blockCbor}
+
+		blockRes, err := client.GetBlock(context.Background(), 1)
+		require.NoError(t, err)
+		require.Equal(t, block, blockRes)
+	})
+	t.Run("GetBlock_NOK", func(t *testing.T) {
+		service.reset()
+		service.err = errors.New("some error")
+
+		block, err := client.GetBlock(context.Background(), 1)
+		require.ErrorContains(t, err, "some error")
+		require.Nil(t, block)
 	})
 }
 
 func startServerAndClient(t *testing.T, service *mockService) *Client {
-	server := rpc.NewServer()
+	server := ethrpc.NewServer()
 	t.Cleanup(server.Stop)
 
 	err := server.RegisterName("state", service)
@@ -57,10 +189,46 @@ func startServerAndClient(t *testing.T, service *mockService) *Client {
 }
 
 type mockService struct {
-	roundNumber    uint64
-	roundNumberErr error
+	roundNumber  uint64
+	unitData     *rpc.UnitData
+	ownerUnitIds []types.UnitID
+	txProof      *rpc.TransactionRecordAndProof
+	block        *rpc.Block
+	err          error
 }
 
 func (s *mockService) GetRoundNumber() (uint64, error) {
-	return s.roundNumber, s.roundNumberErr
+	return s.roundNumber, s.err
+}
+
+func (s *mockService) GetUnit(unitID []byte, returnProof, returnData bool) (*rpc.UnitData, error) {
+	return s.unitData, s.err
+}
+
+func (s *mockService) GetUnitsByOwnerID(ownerID []byte) ([]types.UnitID, error) {
+	return s.ownerUnitIds, s.err
+}
+
+func (s *mockService) SendTransaction(tx *rpc.Transaction) ([]byte, error) {
+	var txo *types.TransactionOrder
+	if err := cbor.Unmarshal(tx.TxOrderCbor, &txo); err != nil {
+		return nil, err
+	}
+	return txo.Hash(crypto.SHA256), s.err
+}
+
+func (s *mockService) GetTransactionProof(txHash []byte) (*rpc.TransactionRecordAndProof, error) {
+	return s.txProof, s.err
+}
+
+func (s *mockService) GetBlock(roundNumber uint64) (*rpc.Block, error) {
+	return s.block, s.err
+}
+
+func (s *mockService) reset() {
+	s.roundNumber = 0
+	s.unitData = nil
+	s.ownerUnitIds = nil
+	s.txProof = nil
+	s.err = nil
 }
