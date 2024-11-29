e3a735684af78c307ee0210336e4144a96320688
Pavel Grigorenko <pavel.grigorenko@guardtime.com>
2024-11-29 01:23:30 +0100

    clean-up

--- internal/testutils/tx.go
+++ internal/testutils/tx.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// testutils.TxHash(t, tx)
 func TxHash(t *testing.T, tx *types.TransactionOrder) []byte {
 	hash, err := tx.Hash(crypto.SHA256)
 	require.NoError(t, err)
